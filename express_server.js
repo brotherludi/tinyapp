f8eb38f70b4f1492579427ada14bbd4260f0cb1a
Di Lu <brotherludi@hotmail.com>
2022-12-01 20:14:59 -0700

    Redirect After Form Submission

--- express_server.js
+++ express_server.js
@@ -9,11 +9,37 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+function generateRandomString() {
+  const lowerCase = 'abcdefghijklmnopqrstuvwxyz';
+  const upperCase = lowerCase.toUpperCase();
+  const numeric = '1234567890';
+  const alphaNumeric = lowerCase + upperCase + numeric;
+  //alphaNumeric is 62
+  let index = Math.round(Math.random() * 100);
+  if (index > 61) {
+    while (index > 61) {
+      index = Math.round(Math.random() * 100);
+    }
+  }
+  return alphaNumeric[index];
+}
+
+const generateShortURL = () => {
+  let randomString = '';
+  while (randomString.length < 6) {
+    randomString += generateRandomString();
+  }
+  return randomString;
+}
+
 app.use(express.urlencoded({ extended: true }));
 
 app.post("/urls", (req, res) => {
   console.log(req.body); // Log the POST request body to the console
-  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+  const shortURL = generateShortURL()
+  const longURL = req.body.longURL
+  urlDatabase[shortURL] = longURL
+  res.redirect("/urls"); 
 });
 
 app.get("/", (req, res) => {
@@ -38,7 +64,7 @@ app.get("/urls/new", (req, res) => {
 });
 
 app.get("/urls/:id", (req, res) => {
-  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id]/* What goes here? */ };
+  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id]};
   res.render("urls_show", templateVars);
 });
 
